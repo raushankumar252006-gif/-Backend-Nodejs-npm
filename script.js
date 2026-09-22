b501ff2da376b857c8417e45a5f63d6a29c588e9
Raushan Kumar <raushankumar252006@gmail.com>
2026-09-22 13:08:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// let n = 5;
+
+// for( let i=0; i<n; i ++){
+//     console.log("hello,",i);
+// }
+
+// console.log("raushan");
+
+
+// console.log(process.argv);
+
+const someValue = require("./math");
+
+console.log(someValue);
